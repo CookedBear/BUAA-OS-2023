a21546c59b8b2f71e67d86b1d33d3b425ef64e46
孙奕博 <21371068@buaa.edu.cn>
2023-02-16 17:28:15 +0800

    test1

--- blib.c
+++ blib.c
@@ -1,11 +1,30 @@
 #include <blib.h>
 
 size_t strlen(const char *s) {
-	panic("please implement");
+	//panic("please implement");
+	size_t l = 0;
+	char* p = (char*)s;
+	while(*p != '\0'){
+		l++;
+		p = p + 1;
+	}
+	return l;
 }
 
 char *strcpy(char *dst, const char *src) {
-	panic("please implement");
+	//panic("please implement");
+	char* p1 = dst;
+	char* p2 = (char*)src;
+	if(p1 == NULL || p2 == NULL){
+		return NULL;
+	}
+	while(*p2 != '\0'){
+		*p1 = *p2;
+		p1++;
+		p2++;	
+	}
+	*p1 = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -18,11 +37,32 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-	panic("please implement");
+	//panic("please implement");
+	char* p1 = dst;
+	while(*p1 != '\0'){
+		p1++;
+	}
+	char* p2 = (char*)src;
+	while(*p2 != '\0'){
+		*p1++ = *p2++;
+	}
+	*p1 = '\0';
+	p1 = dst;
+	return p1;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	panic("please implement");
+	//panic("please implement");
+	char* s11 = (char*)s1;
+	char* s12 = (char*)s2;
+	int flag = 0;	
+	while((flag=*s11-*s12) == 0 && *s11 != '\0'){
+		s11++;
+		s12++;
+	}
+	if(flag < 0) return -1;
+	else if(flag > 0) return 1;
+	else return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -40,7 +80,15 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	panic("please implement");
+	//panic("please implement");
+	if(n < 0 || s == NULL){
+		return NULL;
+	}
+	char* p1 = s;
+	while(n-- > 0){
+		*p1++ = c;
+	}
+	return s;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
@@ -53,5 +101,17 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-	panic("please implement");
+	//panic("please implement");
+	char* s11 = (char*)s1;
+	char* s12 = (char*)s2;
+
+	int flag = *s11 - *s12;
+	while(flag == 0 && --n > 0){
+		s11++;
+		s12++;
+		flag = *s11 - *s12;
+	}
+	if(flag > 0) return 1;
+	else if(flag < 0) return -1;
+	else return 0;
 }
