42399c59c3f81b3b674dd630d1d5bd7224c925da
孙奕博 <21371068@buaa.edu.cn>
2023-03-06 19:46:41 +0800

    3

--- analyze.sh
+++ analyze.sh
@@ -3,16 +3,18 @@
 if [ $# -eq 1 ];
 then
     # Your code here. (1/4)
-
+	grep -E "WARN|ERROR" mos.log > bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-
+	tail -n 5 mos.log 
     ;;
     "--find")
         # Your code here. (3/4)
-
+	str=$3
+	touch $3.txt
+	grep  $3 $1 > $3.txt
     ;;
     "--diff")
         # Your code here. (4/4)
