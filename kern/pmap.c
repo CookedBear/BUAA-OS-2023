309b7b18680a1b34c10e271a8d1f53b3a9bd15fb
孙奕博 <21371068@buaa.edu.cn>
2023-04-04 16:27:09 +0000

    更新 kern/pmap.c

--- kern/pmap.c
+++ kern/pmap.c
@@ -526,3 +526,139 @@ void page_check(void) {
 
   printk("page_check() succeeded!\n");
 }
+
+#include <swap.h>
+
+struct Page_list page_free_swapable_list;
+static u_char *disk_alloc();
+static void disk_free(u_char *pdisk);
+
+void swap_init() {
+  LIST_INIT(&page_free_swapable_list);
+  for (int i = SWAP_PAGE_BASE; i < SWAP_PAGE_END; i += BY2PG) {
+    struct Page *pp = pa2page(i);
+    LIST_REMOVE(pp, pp_link);
+    LIST_INSERT_HEAD(&page_free_swapable_list, pp, pp_link);
+  }
+}
+
+// Interface for 'Passive Swap Out'
+struct Page *swap_alloc(Pde *pgdir, u_int asid) {
+  // Step 1: Ensure free page
+  if (LIST_EMPTY(&page_free_swapable_list)) {
+    /* Your Code Here (1/3) */
+    int pagecnt = asid % 16;
+    struct Page *pp = pa2page(0x3900000) + pagecnt;
+    u_char *da = disk_alloc();
+
+    Pde *pgdirr;
+    Pte *ptebase;
+    Pte *ptdir;
+    for (int i = 0; i < 1024; i++) {
+      pgdirr = pgdir + i;
+      if ((pgdirr != 0) && (*pgdirr & PTE_V)) {
+        ptebase = (Pte *)KADDR(PTE_ADDR(*pgdirr));
+        for (int j = 0; j < 1024; j++) {
+          ptdir = ptebase + j;
+          if ((ptdir != 0) && (*ptdir & PTE_V) &&
+              PTE_ADDR(*ptdir) == PTE_ADDR(page2pa(pp))) {
+            *ptdir = *ptdir & (~PTE_V);
+            *ptdir = *ptdir | PTE_SWP;
+            *ptdir = (u_long)da | (*ptdir & 0xfff);
+            // memcpy(da, (const void *)page2kva(pp), BY2PG);
+            tlb_invalidate(asid,
+                           (u_long)(i << PDSHIFT) + (u_long)(j << PGSHIFT));
+          }
+        }
+      }
+    }
+    memcpy((void *)da, (const void *)page2kva(pp), BY2PG);
+    LIST_INSERT_HEAD(&page_free_swapable_list, pp, pp_link);
+    // return pp;
+  }
+
+  // Step 2: Get a free page and clear it
+  struct Page *pp = LIST_FIRST(&page_free_swapable_list);
+  LIST_REMOVE(pp, pp_link);
+  memset((void *)page2kva(pp), 0, BY2PG);
+
+  return pp;
+}
+
+// Interfaces for 'Active Swap In'
+static int is_swapped(Pde *pgdir, u_long va) { /* Your Code Here (2/3) */
+  Pte *pte;
+  pgdir_walk(pgdir, va, 0, &pte);
+  if ((pte != 0) && (*pte & PTE_SWP)) {
+    return 1;
+  }
+  return 0;
+}
+
+static void swap(Pde *pgdir, u_int asid, u_long va) {
+
+  /* Your Code Here (3/3) */
+
+  struct Page *pp = swap_alloc(pgdir, asid);
+  Pte *ptte;
+  pgdir_walk(pgdir, va, 0, &ptte);
+  u_long da = PTE_ADDR(*ptte);
+  memcpy((void *)page2kva(pp), (const void *)da, BY2PG);
+  Pde *pde = pgdir;
+  Pte *pte;
+  Pte *ptebase;
+  for (int i = 0; i < 1024; i++) {
+    pde = pgdir + i;
+    if ((pde != 0) && (*pde & PTE_V)) {
+      ptebase = (Pte *)KADDR(PTE_ADDR(*pde));
+      pte = ptebase;
+      for (int j = 0; j < 1024; j++) {
+        pte = ptebase + j;
+        if ((pte != 0) && (*pte & PTE_SWP) && ~(*pte & PTE_V)) {
+          if (PTE_ADDR(*pte) == da) {
+            *pte = *pte | PTE_V;
+            *pte = *pte & ~(PTE_SWP);
+            *pte = (Pte)(page2pa(pp)) | (*pte & 0xfff);
+            tlb_invalidate(asid, (u_long)(i << 22) + (u_long)(j << 12));
+          }
+        }
+      }
+    }
+  }
+  disk_free((u_char *)da);
+  // tlb_invalidate(asid, va);
+}
+
+Pte swap_lookup(Pde *pgdir, u_int asid, u_long va) {
+  // Step 1: If corresponding page is swapped out, swap it in
+  if (is_swapped(pgdir, va)) {
+    swap(pgdir, asid, va);
+  }
+
+  // Step 2: Look up page table element.
+  Pte *ppte;
+  page_lookup(pgdir, va, &ppte);
+
+  // Step 3: Return
+  return ppte == NULL ? 0 : *ppte;
+}
+
+// Disk Simulation (Do not modify)
+u_char swap_disk[SWAP_DISK_NPAGE * BY2PG] __attribute__((aligned(BY2PG)));
+u_char swap_disk_used[SWAP_DISK_NPAGE];
+
+static u_char *disk_alloc() {
+  int alloc = 0;
+  for (; alloc < SWAP_DISK_NPAGE && swap_disk_used[alloc]; alloc++) {
+    ;
+  }
+  assert(alloc < SWAP_DISK_NPAGE);
+  swap_disk_used[alloc] = 1;
+  return &swap_disk[alloc * BY2PG];
+}
+
+static void disk_free(u_char *pdisk) {
+  int offset = pdisk - swap_disk;
+  assert(offset % BY2PG == 0);
+  swap_disk_used[offset / BY2PG] = 0;
+}
