9570b88567ffb28a1b140c0e3bcaed752878cee0
孙奕博 <21371068@buaa.edu.cn>
2023-05-22 20:50:26 +0800

    first

--- user/include/fs.h
+++ user/include/fs.h
@@ -24,14 +24,15 @@
 #define BY2FILE 256
 
 struct File {
-	char f_name[MAXNAMELEN]; // filename
-	uint32_t f_size;	 // file size in bytes
-	uint32_t f_type;	 // file type
-	uint32_t f_direct[NDIRECT];
-	uint32_t f_indirect;
+  char f_name[MAXNAMELEN]; // filename
+  uint32_t f_size;         // file size in bytes
+  uint32_t f_type;         // file type
+  uint32_t f_direct[NDIRECT];
+  uint32_t f_indirect;
 
-	struct File *f_dir; // the pointer to the dir where this file is in, valid only in memory.
-	char f_pad[BY2FILE - MAXNAMELEN - (3 + NDIRECT) * 4 - sizeof(void *)];
+  struct File *f_dir; // the pointer to the dir where this file is in, valid
+                      // only in memory.
+  char f_pad[BY2FILE - MAXNAMELEN - (3 + NDIRECT) * 4 - sizeof(void *)];
 } __attribute__((aligned(4), packed));
 
 #define FILE2BLK (BY2BLK / sizeof(struct File))
@@ -39,15 +40,16 @@ struct File {
 // File types
 #define FTYPE_REG 0 // Regular file
 #define FTYPE_DIR 1 // Directory
+#define FTYPE_LNK 2
 
 // File system super-block (both in-memory and on-disk)
 
 #define FS_MAGIC 0x68286097 // Everyone's favorite OS class
 
 struct Super {
-	uint32_t s_magic;   // Magic number: FS_MAGIC
-	uint32_t s_nblocks; // Total number of blocks on disk
-	struct File s_root; // Root directory node
+  uint32_t s_magic;   // Magic number: FS_MAGIC
+  uint32_t s_nblocks; // Total number of blocks on disk
+  struct File s_root; // Root directory node
 };
 
 #endif // _FS_H_
