7257dd5d08037c8b2d1ea765889ef82e812527cc
孙奕博 <21371068@buaa.edu.cn>
2023-05-22 18:48:50 +0800

    temp

--- user/include.mk
+++ user/include.mk
@@ -1,6 +1,6 @@
 lab-ge = $(shell [ "$$(echo $(lab)_ | cut -f1 -d_)" -ge $(1) ] && echo true)
 
-INITAPPS             := tltest.x fktest.x pingpong.x tree.x
+INITAPPS             := tltest.x fktest.x pingpong.x
 
 USERLIB              := entry.o \
 			syscall_wrap.o \
