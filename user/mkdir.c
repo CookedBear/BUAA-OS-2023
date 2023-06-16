a659912b8abb21eff41bb9d03c94417472cca7e2
孙奕博 <21371068@buaa.edu.cn>
2023-06-16 19:09:13 +0800

    fix output in mkdir

--- user/mkdir.c
+++ user/mkdir.c
@@ -16,12 +16,7 @@ int main(int argc, char **argv) {
         if ((r = mkdir(argv[1])) < 0) {
             printf("create path fail!\n");
         } else {
-            char cwd[1024] = {0};
-            getcwd(cwd);
-            printf("created path: %s", cwd);
-            if (strlen(cwd) != 1) {
-                printf("/");
-            }
+            printf("created path: ");
             printf("%s\n", argv[1]);
         }
     }
